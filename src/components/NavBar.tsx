19afd7523faf26513c9d9c95d837167d7b204019
Thing Wei Jen <weijen06@hotmail.com>
2025-09-21 19:41:14 +0800

    fix: updated navbar to replace path instead of adding path to history stack

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,9 @@
-import { Link, useLocation } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import { motion } from "framer-motion";
 
 export default function Navbar() {
   const { pathname } = useLocation();
+  const navigate = useNavigate();
 
   const navItems = [
     { path: "/", label: "Home" },
@@ -13,14 +14,15 @@ export default function Navbar() {
 
   return (
     <nav className="fixed top-0 w-full flex items-center justify-between px-8 bg-black/50 backdrop-blur z-50 py-4">
-      {/* Left side (logo / site name) */}
       <div className="text-white font-bold"></div>
 
-      {/* Center (nav links) */}
       <ul className="absolute left-1/2 -translate-x-1/2 flex gap-6 text-white">
         {navItems.map((item) => (
           <li key={item.path}>
-            <Link to={item.path} className="relative">
+            <button
+              onClick={() => navigate(item.path, { replace: true })}
+              className="relative"
+            >
               {item.label}
               {pathname === item.path && (
                 <motion.div
@@ -28,7 +30,7 @@ export default function Navbar() {
                   className="absolute left-0 right-0 h-[2px] bg-white bottom-[-4px]"
                 />
               )}
-            </Link>
+            </button>
           </li>
         ))}
       </ul>
